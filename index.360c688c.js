0b8f531665020f57a1092128d9c686b1653266d9
Ruslan Kasinets <rulik.357@gmail.com>
2025-11-18 19:28:20 +0200

    make build

---
+++ index.360c688c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=e.clientHeight,l=e.clientWidth,i=t.clientHeight,n=t.clientWidth;e.style.left="".concat((n-l)/2,"px"),e.style.top="".concat((i-c)/2,"px");
+//# sourceMappingURL=index.360c688c.js.map
